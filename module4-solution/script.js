3f79f7de285bda1a2da41fdd8a6c44a1aa59a8b8
Aline <cadettealine69@gmail.com>
2021-02-01 22:48:14 +0200

    our Menu.

---
+++ module4-solution/script.js
@@ -0,0 +1,13 @@
+(function(name){
+var = {Yaakov,John,Jen,jason,Frank,Larry,Paula};
+
+for (var i = 0; i < Things.length= i++) {
+	Things[i];
+
+var firstLetter = names{i}.charAt(0).ToLowerCase();
+
+   if (firstLetter==='j'){
+   	byeSpeaker.speak(names[i]);
+    else {
+   	helloSpeaker.speak(names[i]);
+
